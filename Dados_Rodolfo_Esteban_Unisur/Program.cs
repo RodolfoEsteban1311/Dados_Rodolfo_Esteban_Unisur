4bf0fbe75f8783f2812595a938edbd7df9a5f3b5
52998 <52998@LAPTOP-5PL3PPH7>
2023-08-31 17:52:33 -0500

    Agregar archivos de proyecto.

---
+++ Dados_Rodolfo_Esteban_Unisur/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados_Rodolfo_Esteban_Unisur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rand = new Random();
+            int d1 = rand.Next(1, 7);
+            int d2 = rand.Next(1, 7);
+            int d3 = rand.Next(1, 7);
+            Console.WriteLine("Dado 1=" + d1);
+            Console.WriteLine("Dado 2=" + d2);
+            Console.WriteLine("Dado 3=" + d3);
+            if (d1 == 6 && d2 == 6 & d3 == 6)
+            {
+                Console.WriteLine("Excelente");
+            }
+            else
+            {
+                if ((d1 == 6 && d2 == 6) || (d1 == 6 && d3 == 6) || (d2 == 6 && d3 == 6))
+                {
+                    Console.WriteLine("Muy bien");
+                }
+                else
+                {
+                    if (d1 == 6 || d2 == 6 || d3 == 6)
+                    {
+                        Console.WriteLine("Bien");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Pesimo");
+                    }
+                }
+            }
+            Console.Read();
+        }
+    }
+}
